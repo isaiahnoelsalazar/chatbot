b11a0ac68864ab722776ecce4f477aa44a5a37bd
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-09-02 18:08:19 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const generateBtn = document.getElementById('generateBtn');
+const textInput = document.getElementById('textInput');
+const generatedTextContainer = document.getElementById('generatedTextContainer');
+
+generateBtn.addEventListener('click', () => {
+    let requestPost = new XMLHttpRequest();
+    requestPost.open("POST", `https://sasasaia.pythonanywhere.com/chatbot?message=${textInput.value}`);
+    requestPost.onreadystatechange = function (){
+        if (requestPost.status == 200 && requestPost.readyState == 4){
+            let response = requestPost.responseText;
+            let p = document.createElement('p');
+            p.innerHTML = response;
+            generatedTextContainer.appendChild(p);
+        }
+    }
+    requestPost.send();
+});

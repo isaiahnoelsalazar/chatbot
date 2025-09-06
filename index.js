e14a5fc34fa4506bb740c3868e931d307595665a
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-09-06 22:18:34 +0800

    Update website design

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const textInput = document.getElementById('textInput');
 const generatedTextContainer = document.getElementById('generatedTextContainer');
 
 generateBtn.addEventListener('click', () => {
+    generatedTextContainer.children.array.forEach(child => {
+        generatedTextContainer.removeChild(child);
+    });
     let requestPost = new XMLHttpRequest();
     requestPost.open("POST", `https://sasasaia.pythonanywhere.com/chatbot?message=${textInput.value}`);
     requestPost.onreadystatechange = function (){

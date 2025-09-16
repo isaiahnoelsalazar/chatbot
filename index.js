5a4ead98693a8f16a435fed1b74df4ee4238c0f8
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-09-16 11:08:57 +0800

    Updated backend server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ generateBtn.addEventListener('click', () => {
     generateBtn.innerHTML = "Generating...";
     generateBtn.disabled = true;
     let requestPost = new XMLHttpRequest();
-    requestPost.open("POST", `https://sasasaia.pythonanywhere.com/chatbot?message=${textInput.value}`);
+    requestPost.open("POST", `https://sharenetarchive1.pythonanywhere.com/chatbot?message=${textInput.value}`);
     requestPost.onreadystatechange = function (){
         if (requestPost.status == 200 && requestPost.readyState == 4){
             while (generatedTextContainer.firstChild){
